4fc9e88f7d2bc64ae55c4daa18468c6c70e7ea39
tanayvaswani <vaswani.tanay9@gmail.com>
2024-05-26 20:42:04 +0530

    chore: attr-set

---
+++ attr-set.nix
@@ -0,0 +1,14 @@
+# Attribute Set -- Normal
+{
+  one = 1;
+  two = 2;
+  three = 3;
+}
+
+
+# Attribute Set -- without access to inner var
+#{
+#  one = 1;
+#  two = one + 1;
+#  three = two + 1;
+#}
